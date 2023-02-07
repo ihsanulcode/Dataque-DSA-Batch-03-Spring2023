671cde7e54ad9a2133d7068bcc600f85770bc5ce
Ihsanul Haque Asif <66785935+ihsanulcode@users.noreply.github.com>
2023-02-07 22:04:16 +0600

    Class-01-Memory-Variables-Basic_C++

---
+++ Class-01/hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "This is our class-01" << endl;
+    cout << "This is our 1st program" << endl;
+
+    cout << "Hi" << endl << "hello" << endl << "hey" << endl;
+    cout << "Hi " << "hello " << "hey" << endl;
+
+    return 0;
+}
---
+++ Class-01/variables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Ihsanul Haque";
+    int id = 111;
+    double cgpa = 3.9;
+    char grade = 'A';
+    bool isGraduate = false;
+
+    cout << name << endl;
+    cout << id << endl;
+    cout << cgpa << endl;
+    cout << grade << endl;
+    cout << isGraduate << endl;
+
+}
